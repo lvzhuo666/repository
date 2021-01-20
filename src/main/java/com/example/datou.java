707f4943c66814ff6545eacbfada90329e2ae38d
lvzhuo666 <1012939351@qq.com>
2021-01-20 19:29:57 +0800

    大头

--- src/main/java/com/example/datou.java
+++ src/main/java/com/example/datou.java
@@ -5,5 +5,6 @@ public class datou {
     public static void main(String[] args) {
         System.out.println("大头牛逼");
         System.out.println("china numb");
+        System.out.println("aadsge");
     }
 }
